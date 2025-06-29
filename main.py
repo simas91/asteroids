3891fa9b812ad3ed28b132c22f48db1b20e985cc
simas91 <simas91@gmail.com>
2025-06-29 13:06:32 +0100

    Finished project

--- main.py
+++ main.py
@@ -1,22 +1,63 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+import sys
 import pygame
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
-    pygame.init()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    # print("Starting Asteroids!")
+    # print(f"Screen width: {SCREEN_WIDTH}")
+    # print(f"Screen height: {SCREEN_HEIGHT}")
 
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    Shot.containers = (shots, updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
+
+    updatable.add(player)
+    drawable.add(player)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black", rect=None, special_flags=0)
-        pygame.display.flip()
+        
+        updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.coliding(player):
+                print("Game over!")
+                sys.exit()
+            for bullet in shots:
+                if asteroid.coliding(bullet):
+                    bullet.kill()
+                    asteroid.split()
+
+        screen.fill("black") # fill the screen
+
+        for item in drawable:
+            item.draw(screen)
+
+        pygame.display.flip() # refrech the screen
+        dt = clock.tick(60) / 1000 # limit to 60 fps
 
 if __name__ == "__main__":
     main()
